4fe2371e3211d7505da59bc556ede80208932b62
harshit.panwar <harshit.panwar@namastecredit.com>
2024-07-08 02:26:34 +0530

    env changes

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -21,7 +21,7 @@ exports.login = async (req, res) => {
         }
 
         const token = generateToken(user._id, user.role);
-        res.cookie('token', token, { httpOnly: true, secure: true });
+        res.cookie('token', token, { httpOnly: true, secure: true, sameSite: 'None' });
         res.json({ 
             token,
             role: user.role,
